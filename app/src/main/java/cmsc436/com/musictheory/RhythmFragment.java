076f9eff7bfae350fc1c0b088714893ad142df90
adweed93 <adweed@terpmail.umd.edu>
2016-05-12 00:11:24 -0400

    Fully Functional V1

--- app/src/main/java/cmsc436/com/musictheory/RhythmFragment.java
+++ app/src/main/java/cmsc436/com/musictheory/RhythmFragment.java
@@ -1,11 +1,14 @@
 package cmsc436.com.musictheory;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 
 /**
@@ -13,9 +16,13 @@ import android.view.ViewGroup;
  */
 public class RhythmFragment extends Fragment {
 
+    ImageButton play_stop_button;
+    ImageView measure_display;
+    private View myFragmentView;
 
     public RhythmFragment() {
         // Required empty public constructor
+
     }
 
 
@@ -23,7 +30,63 @@ public class RhythmFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.rhythm_fragment, container, false);
+        myFragmentView = inflater.inflate(R.layout.rhythm_fragment, container, false);
+
+        Bundle bundle = getArguments();
+        String button_value = bundle.getString("button_value");
+
+        measure_display = (ImageView) myFragmentView.findViewById(R.id.measure_image);
+
+        if (button_value.equals("whole")) {
+            measure_display.setImageResource(R.drawable.whole_note_measure);
+        }else if (button_value.equals("half")){
+            measure_display.setImageResource(R.drawable.half_note_measure);
+        }else if (button_value.equals("quarter")){
+            measure_display.setImageResource(R.drawable.quarter_note_measure);
+        }else if (button_value.equals("eighth")){
+            measure_display.setImageResource(R.drawable.eighth_note_measure);
+        }else if (button_value.equals("sixteenth")){
+            measure_display.setImageResource(R.drawable.sixteenth_note_measure);
+        }
+
+        play_stop_button = (ImageButton) myFragmentView.findViewById(R.id.play_stop_button);
+
+        play_stop_button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Bundle bundle = getArguments();
+                String button_value = bundle.getString("button_value");
+                MediaPlayer mp;
+
+
+
+                if(button_value.equals("whole")) {
+                    mp = MediaPlayer.create(getActivity().getBaseContext(), R.raw.whole_note);
+                }else if (button_value.equals("half")){
+                    mp = MediaPlayer.create(getActivity().getBaseContext(), R.raw.half_note);
+                }else if (button_value.equals("quarter")){
+                    mp = MediaPlayer.create(getActivity().getBaseContext(), R.raw.quarter_note);
+                }else if (button_value.equals("eighth")){
+                    mp = MediaPlayer.create(getActivity().getBaseContext(), R.raw.eighth_note);
+                }else if (button_value.equals("sixteenth")){
+                    mp = MediaPlayer.create(getActivity().getBaseContext(), R.raw.sixteenth_note);
+                }else {
+                    mp = MediaPlayer.create(getActivity().getBaseContext(), R.raw.blank_120);
+                }
+                mp.start();
+
+                while(mp.isPlaying())
+                {
+                    //Do nothing
+                }
+
+                mp.release();
+            }
+        });
+
+
+
+        return myFragmentView;
     }
 
 }
--- app/src/main/java/cmsc436/com/musictheory/RhythmReference.java
+++ app/src/main/java/cmsc436/com/musictheory/RhythmReference.java
@@ -14,6 +14,15 @@ import android.widget.ImageButton;
 public class RhythmReference extends AppCompatActivity {
 
     ImageButton wholeNoteButton;
+    ImageButton wholeRestButton;
+    ImageButton halfNoteButton;
+    ImageButton halfRestButton;
+    ImageButton quarterNoteButton;
+    ImageButton quarterRestButton;
+    ImageButton eighthNoteButton;
+    ImageButton eighthRestButton;
+    ImageButton sixteenthNoteButton;
+    ImageButton sixteenthRestButton;
     FragmentManager fragmentManager = getFragmentManager();
 
     @Override
@@ -23,20 +32,197 @@ public class RhythmReference extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Bundle bundle = new Bundle();
+        bundle.putString("button_value", "blank");
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        RhythmFragment rhythmFragment = new RhythmFragment();
+        rhythmFragment.setArguments(bundle);
+        fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
         wholeNoteButton = (ImageButton) findViewById(R.id.whole_note_button);
         wholeNoteButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
 
+                Bundle bundle = new Bundle();
+                bundle.putString("button_value", "whole");
 
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                     RhythmFragment rhythmFragment = new RhythmFragment();
+                    rhythmFragment.setArguments(bundle);
                     fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
                     fragmentTransaction.addToBackStack(null);
                     fragmentTransaction.commit();
 
             }
         });
+
+        wholeRestButton = (ImageButton) findViewById(R.id.whole_rest_button);
+        wholeRestButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("button_value", "whole");
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                RhythmFragment rhythmFragment = new RhythmFragment();
+                rhythmFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        halfNoteButton = (ImageButton) findViewById(R.id.half_note_button);
+        halfNoteButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("button_value", "half");
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                RhythmFragment rhythmFragment = new RhythmFragment();
+                rhythmFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        halfRestButton = (ImageButton) findViewById(R.id.half_rest_button);
+        halfRestButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("button_value", "half");
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                RhythmFragment rhythmFragment = new RhythmFragment();
+                rhythmFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        quarterNoteButton = (ImageButton) findViewById(R.id.quarter_note_button);
+        quarterNoteButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("button_value", "quarter");
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                RhythmFragment rhythmFragment = new RhythmFragment();
+                rhythmFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        quarterRestButton = (ImageButton) findViewById(R.id.quarter_rest_button);
+        quarterRestButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("button_value", "quarter");
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                RhythmFragment rhythmFragment = new RhythmFragment();
+                rhythmFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        eighthNoteButton = (ImageButton) findViewById(R.id.eighth_note_button);
+        eighthNoteButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("button_value", "eighth");
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                RhythmFragment rhythmFragment = new RhythmFragment();
+                rhythmFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        eighthRestButton = (ImageButton) findViewById(R.id.eighth_rest_button);
+        eighthRestButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("button_value", "eighth");
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                RhythmFragment rhythmFragment = new RhythmFragment();
+                rhythmFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        sixteenthNoteButton = (ImageButton) findViewById(R.id.sixteenth_note_button);
+        sixteenthNoteButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("button_value", "sixteenth");
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                RhythmFragment rhythmFragment = new RhythmFragment();
+                rhythmFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        sixteenthRestButton = (ImageButton) findViewById(R.id.sixteenth_rest_button);
+        sixteenthRestButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("button_value", "sixteenth");
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                RhythmFragment rhythmFragment = new RhythmFragment();
+                rhythmFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.rhythm_fragment_container, rhythmFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+
     }
 
     @Override
